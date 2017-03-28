34f9b91268442bd86ae154fb3740d2681af36974
Daniel Hoelbling-Inzko <daniel@hoelbling-inzko.com>
2017-03-28 10:56:49 +0200

    Renamed agentRunCommand to agentDeploymentDownloadUrl

--- bitmovin/resources/models/infrastructures/kubernetes_infrastructure.py
+++ bitmovin/resources/models/infrastructures/kubernetes_infrastructure.py
@@ -4,11 +4,11 @@ from . import AbstractInfrastructure
 
 class KubernetesInfrastructure(AbstractInfrastructure, Serializable):
 
-    def __init__(self, name, description=None, online=None, connected=None, agent_run_command=None, id_=None, custom_data=None):
+    def __init__(self, name, description=None, online=None, connected=None, agent_deployment_download_url=None, id_=None, custom_data=None):
         super().__init__(id_=id_, custom_data=custom_data, name=name, description=description)
         self.online = online
         self.connected = connected
-        self.agentRunCommand = agent_run_command
+        self.agentDeploymentDownloadUrl = agent_deployment_download_url
 
     @classmethod
     def parse_from_json_object(cls, json_object):
@@ -17,10 +17,10 @@ class KubernetesInfrastructure(AbstractInfrastructure, Serializable):
         description = json_object.get('description')
         online = json_object.get('online')
         connected = json_object.get('connected')
-        agent_run_command = json_object.get('agentDeploymentDownloadUrl')
+        agent_deployment_download_url = json_object.get('agentDeploymentDownloadUrl')
 
         kubernetes_infrastructure = KubernetesInfrastructure(name=name, description=description, online=online,
-                                                             connected=connected, agent_run_command=agent_run_command,
+                                                             connected=connected, agent_deployment_download_url=agent_deployment_download_url,
                                                              id_=id_)
 
         return kubernetes_infrastructure
