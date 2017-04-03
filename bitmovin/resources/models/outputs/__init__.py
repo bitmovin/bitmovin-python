3dcfaf49e4b82a67ffbe40b913a06da877812abe
dominic-miglar <dominic.miglar@netunix.at>
2017-04-03 16:05:13 +0200

    added GenericS3Output class

--- bitmovin/resources/models/outputs/__init__.py
+++ bitmovin/resources/models/outputs/__init__.py
@@ -4,3 +4,4 @@ from .gcs_output import GCSOutput
 from .azure_output import AzureOutput
 from .ftp_output import FTPOutput
 from .sftp_output import SFTPOutput
+from .generic_s3_output import GenericS3Output
---
+++ bitmovin/resources/models/outputs/generic_s3_output.py
@@ -0,0 +1,33 @@
+from bitmovin.utils import Serializable
+from . import AbstractOutput
+
+
+class GenericS3Output(AbstractOutput, Serializable):
+
+    def __init__(self, access_key, secret_key, bucket_name, host, port=None, id_=None, custom_data=None,
+                 name=None, description=None):
+        super().__init__(id_=id_, custom_data=custom_data, name=name, description=description)
+        self.accessKey = access_key
+        self.secretKey = secret_key
+        self.bucketName = bucket_name
+        self.host = host
+        self.port = port
+
+    @classmethod
+    def parse_from_json_object(cls, json_object):
+        id_ = json_object['id']
+        bucket_name = json_object['bucketName']
+        access_key = json_object.get('accessKey')
+        secret_key = json_object.get('secretKey')
+        name = json_object.get('name')
+        description = json_object.get('description')
+        host = json_object.get('host')
+        port = json_object.get('port')
+        generic_s3_output = GenericS3Output(
+            access_key=access_key, secret_key=secret_key, bucket_name=bucket_name, host=host, port=port, id_=id_,
+            name=name, description=description)
+        return generic_s3_output
+
+    def serialize(self):
+        serialized = super().serialize()
+        return serialized
