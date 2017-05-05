9a40c14158bfd8b4b9135d2fca704902eb7f8163
Alexander Kopper <alexander.kopper@bitmovin.com>
2017-05-05 11:45:30 +0200

    added example for kubernetes live stream with hls manifest

---
+++ examples/encoding/live/start_live_encoding_on_infrastructure_hls_manifest.py
@@ -0,0 +1,291 @@
+import time, sys, uuid
+from bitmovin import Bitmovin, Encoding, S3Output, H264CodecConfiguration, \
+    AACCodecConfiguration, H264Profile, StreamInput, SelectionMode, Stream, EncodingOutput, ACLEntry, ACLPermission, \
+    FMP4Muxing, MuxingStream, CloudRegion, DashManifest, VideoAdaptationSet, AudioAdaptationSet, Period, \
+    FMP4Representation, FMP4RepresentationType, LiveStreamConfiguration, LiveDashManifest, GenericS3Output, TSMuxing, LiveHlsManifest, HlsManifest, AudioMedia, VariantStream
+from bitmovin.errors import BitmovinApiError
+
+ERROR_CODE_LIVE_STREAM_NOT_READY = 2023
+LIVE_STREAM_INFORMATION_FETCH_RETRY_INTERVAL = 5
+LIVE_STREAM_INFORMATION_FETCH_MAX_RETRIES = 60
+
+API_KEY = '<YOUR_API_KEY>'
+STREAM_KEY = '<YOUR_STREAM_KEY>'
+ENCODER_VERSION = 'STABLE'
+CLOUD_REGION = CloudRegion.KUBERNETES
+INFRASTRUCTURE_ID = '<YOUR_INFRASTRUCTURE_ID>'
+
+GENERIC_S3_OUTPUT_ACCESSKEY = '<YOUR_GENERIC_S3_ACCESS_KEY>'
+GENERIC_S3_OUTPUT_SECRETKEY = '<YOUR_GENERIC_S3_OUTPUT_SECRETKEY>'
+GENERIC_S3_OUTPUT_BUCKETNAME = '<YOUR_GENERIC_S3_OUTPUT_BUCKETNAME>'
+GENERIC_S3_OUTPUT_HOST = '<YOUR_GENERIC_S3_HOST>'
+GENERIC_S3_OUTPUT_PORT = 8080 # YOUR_GENERIC_S3_PORT
+
+OUTPUT_BASE_PATH = 'your/output/base/path/{}/'.format(uuid.uuid4())
+
+class NoRtmpInputAvailableError(Exception):
+    def __init__(self, message):
+        super(message)
+
+def main():
+    bitmovin = Bitmovin(api_key=API_KEY)
+    resource_response = bitmovin.inputs.RTMP.list()
+
+    if not resource_response or \
+       not resource_response.resource or \
+       not isinstance(resource_response.resource, list) or \
+       len(resource_response.resource) < 1:
+        print('Could not find any RTMP inputs. Please contact support.')
+        sys.exit(3)
+
+    rtmp_input_id = resource_response.resource[0].id
+    print("Found RTMP input with ID '{}'".format(rtmp_input_id))
+
+    rtmp_input = bitmovin.inputs.RTMP.retrieve(id_=rtmp_input_id).resource
+
+    output = GenericS3Output(access_key=GENERIC_S3_OUTPUT_ACCESSKEY,
+                         secret_key=GENERIC_S3_OUTPUT_SECRETKEY,
+                         bucket_name=GENERIC_S3_OUTPUT_BUCKETNAME,
+                         host=GENERIC_S3_OUTPUT_HOST,
+                         port=GENERIC_S3_OUTPUT_PORT,
+                         name='Sample Generic S3 Output')
+    output = bitmovin.outputs.GenericS3.create(output).resource
+
+    encoding = Encoding(name='example python hls live stream encoding',
+                        encoder_version=ENCODER_VERSION,
+                        cloud_region=CLOUD_REGION,
+                        infrastructure_id=INFRASTRUCTURE_ID)
+    encoding = bitmovin.encodings.Encoding.create(encoding).resource
+
+    video_codec_configuration_1080p = H264CodecConfiguration(name='example_video_codec_configuration_1080p',
+                                                             bitrate=4800000,
+                                                             rate=25.0,
+                                                             width=1920,
+                                                             height=1080,
+                                                             profile=H264Profile.HIGH)
+    video_codec_configuration_1080p = bitmovin.codecConfigurations.H264.create(video_codec_configuration_1080p).resource
+
+    video_codec_configuration_720p = H264CodecConfiguration(name='example_video_codec_configuration_720p',
+                                                            bitrate=2400000,
+                                                            rate=25.0,
+                                                            width=1280,
+                                                            height=720,
+                                                            profile=H264Profile.HIGH)
+    video_codec_configuration_720p = bitmovin.codecConfigurations.H264.create(video_codec_configuration_720p).resource
+
+    video_codec_configuration_360p = H264CodecConfiguration(name='example_video_codec_configuration_720p',
+                                                            bitrate=800000,
+                                                            rate=25.0,
+                                                            width=640,
+                                                            height=360,
+                                                            profile=H264Profile.HIGH)
+    video_codec_configuration_360p = bitmovin.codecConfigurations.H264.create(video_codec_configuration_360p).resource
+
+    audio_codec_configuration = AACCodecConfiguration(name='example_audio_codec_configuration_english',
+                                                      bitrate=128000,
+                                                      rate=48000)
+    audio_codec_configuration = bitmovin.codecConfigurations.AAC.create(audio_codec_configuration).resource
+
+    video_input_stream = StreamInput(input_id=rtmp_input.id,
+                                     input_path='live',
+                                     position=0,
+                                     selection_mode=SelectionMode.AUTO)
+    audio_input_stream = StreamInput(input_id=rtmp_input.id,
+                                     input_path='live',
+                                     position=1,
+                                     selection_mode=SelectionMode.AUTO)
+
+    video_stream_1080p = Stream(codec_configuration_id=video_codec_configuration_1080p.id,
+                                input_streams=[video_input_stream], name='Sample Stream 1080p')
+    video_stream_1080p = bitmovin.encodings.Stream.create(object_=video_stream_1080p,
+                                                          encoding_id=encoding.id).resource
+
+    video_stream_720p = Stream(codec_configuration_id=video_codec_configuration_720p.id,
+                               input_streams=[video_input_stream], name='Sample Stream 720p')
+    video_stream_720p = bitmovin.encodings.Stream.create(object_=video_stream_720p,
+                                                         encoding_id=encoding.id).resource
+
+    video_stream_360p = Stream(codec_configuration_id=video_codec_configuration_360p.id,
+                               input_streams=[video_input_stream], name='Sample Stream 360p')
+    video_stream_360p = bitmovin.encodings.Stream.create(object_=video_stream_360p,
+                                                         encoding_id=encoding.id).resource
+
+    audio_stream = Stream(codec_configuration_id=audio_codec_configuration.id,
+                          input_streams=[audio_input_stream], name='Sample Stream AUDIO')
+    audio_stream = bitmovin.encodings.Stream.create(object_=audio_stream,
+                                                    encoding_id=encoding.id).resource
+
+    video_muxing_stream_1080p = MuxingStream(video_stream_1080p.id)
+    video_muxing_stream_720p = MuxingStream(video_stream_720p.id)
+    video_muxing_stream_360p = MuxingStream(video_stream_360p.id)
+    audio_muxing_stream = MuxingStream(audio_stream.id)
+
+    acl_entry = ACLEntry(permission=ACLPermission.PUBLIC_READ)
+
+    video_muxing_1080p_output = EncodingOutput(output_id=output.id,
+                                               output_path=OUTPUT_BASE_PATH + 'video/1080p',
+                                               acl=[acl_entry])
+    video_muxing_1080p = TSMuxing(segment_length=4,
+                                    segment_naming='seg_%number%.ts',
+                                    streams=[video_muxing_stream_1080p],
+                                    outputs=[video_muxing_1080p_output],
+                                    name='Sample Muxing 1080p')
+    video_muxing_1080p = bitmovin.encodings.Muxing.TS.create(object_=video_muxing_1080p,
+                                                               encoding_id=encoding.id).resource
+    video_muxing_720p_output = EncodingOutput(output_id=output.id,
+                                              output_path=OUTPUT_BASE_PATH + 'video/720p',
+                                              acl=[acl_entry])
+    video_muxing_720p = TSMuxing(segment_length=4,
+                                   segment_naming='seg_%number%.m4s',
+                                   streams=[video_muxing_stream_720p],
+                                   outputs=[video_muxing_720p_output],
+                                   name='Sample Muxing 720p')
+    video_muxing_720p = bitmovin.encodings.Muxing.TS.create(object_=video_muxing_720p,
+                                                              encoding_id=encoding.id).resource
+
+    video_muxing_360p_output = EncodingOutput(output_id=output.id,
+                                              output_path=OUTPUT_BASE_PATH + 'video/360p',
+                                              acl=[acl_entry])
+    video_muxing_360p = TSMuxing(segment_length=4,
+                                   segment_naming='seg_%number%.m4s',
+                                   streams=[video_muxing_stream_360p],
+                                   outputs=[video_muxing_360p_output],
+                                   name='Sample Muxing 360p')
+    video_muxing_360p = bitmovin.encodings.Muxing.TS.create(object_=video_muxing_360p,
+                                                              encoding_id=encoding.id).resource
+
+    audio_muxing_output = EncodingOutput(output_id=output.id,
+                                              output_path=OUTPUT_BASE_PATH + 'audio/128k',
+                                              acl=[acl_entry])
+    audio_muxing = TSMuxing(segment_length=4,
+                              segment_naming='seg_%number%.m4s',
+                              streams=[audio_muxing_stream],
+                              outputs=[audio_muxing_output],
+                              name='Sample Muxing AUDIO')
+    audio_muxing = bitmovin.encodings.Muxing.TS.create(object_=audio_muxing,
+                                                         encoding_id=encoding.id).resource
+
+    #### Manifest #####################################################################################################
+
+    manifest_output = EncodingOutput(output_id=output.id,
+                                     output_path=OUTPUT_BASE_PATH,
+                                     acl=[acl_entry])
+    hls_manifest = HlsManifest(manifest_name='example_live_manifest.m3u8',
+                                 outputs=[manifest_output],
+                                 name='Sample HLS Manifest Live',
+                                 description='Bitmovin Python Test Live HLS Manifest')
+    hls_manifest = bitmovin.manifests.HLS.create(hls_manifest).resource
+
+    audio_media = AudioMedia(name='Sample Audio Media',
+                             group_id='audio_group',
+                             segment_path="audio/128k/",
+                             encoding_id=encoding.id,
+                             stream_id=audio_stream.id,
+                             muxing_id=audio_muxing.id,
+                             language='en',
+                             uri='audiomedia.m3u8',
+                             autoselect=False)
+
+    audio_media = bitmovin.manifests.HLS.AudioMedia.create(manifest_id=hls_manifest.id, object_=audio_media).resource
+
+    variant_stream_1080p = VariantStream(audio=audio_media.groupId,
+                                         closed_captions='NONE',
+                                         segment_path="video/1080p/",
+                                         uri='video_1080p.m3u8',
+                                         encoding_id=encoding.id,
+                                         stream_id=video_stream_1080p.id,
+                                         muxing_id=video_muxing_1080p.id)
+
+    variant_stream_1080p = bitmovin.manifests.HLS.VariantStream.create(manifest_id=hls_manifest.id,
+                                                                       object_=variant_stream_1080p)
+
+    variant_stream_720p = VariantStream(audio=audio_media.groupId,
+                                        closed_captions='NONE',
+                                        segment_path="video/720p/",
+                                        uri='video_720p.m3u8',
+                                        encoding_id=encoding.id,
+                                        stream_id=video_stream_720p.id,
+                                        muxing_id=video_muxing_720p.id)
+
+    variant_stream_720p = bitmovin.manifests.HLS.VariantStream.create(manifest_id=hls_manifest.id,
+                                                                      object_=variant_stream_720p)
+
+    variant_stream_360p = VariantStream(audio=audio_media.groupId,
+                                        closed_captions='NONE',
+                                        segment_path="video/360p/",
+                                        uri='video_360p.m3u8',
+                                        encoding_id=encoding.id,
+                                        stream_id=video_stream_360p.id,
+                                        muxing_id=video_muxing_360p.id)
+
+    variant_stream_360p = bitmovin.manifests.HLS.VariantStream.create(manifest_id=hls_manifest.id,
+                                                                      object_=variant_stream_360p)
+
+    bitmovin.manifests.HLS.start(manifest_id=hls_manifest.id)
+
+    ###################################################################################################################
+
+    live_hls_manifest = LiveHlsManifest(manifest_id=hls_manifest.id)
+
+    live_stream_configuration = LiveStreamConfiguration(
+        stream_key=STREAM_KEY,
+        live_hls_manifests=[live_hls_manifest]
+    )
+
+    resource_response = bitmovin.encodings.Encoding.start_live(
+        encoding_id=encoding.id,
+        live_stream_configuration=live_stream_configuration
+    )
+
+    bitmovin.encodings.Encoding.wait_until_running(encoding_id=resource_response.resource.id)
+
+    live_details = None
+    retry = 0
+
+    while retry < LIVE_STREAM_INFORMATION_FETCH_MAX_RETRIES:
+        try:
+            live_details = bitmovin.encodings.Encoding.retrieve_live(encoding_id=resource_response.resource.id)
+            break
+        except BitmovinApiError as bitmovin_api_error:
+            if bitmovin_api_error.response.data.code != ERROR_CODE_LIVE_STREAM_NOT_READY:
+                sys.exit(2)
+            print("Live stream is not ready yet. Retrying to fetch live stream information in {} seconds...".format(
+                LIVE_STREAM_INFORMATION_FETCH_RETRY_INTERVAL
+            ))
+            retry += 1
+            time.sleep(LIVE_STREAM_INFORMATION_FETCH_RETRY_INTERVAL)
+
+    if live_details is not None:
+        print_live_stream_details(encoding_id=encoding.id, live_stream_details=live_details.resource)
+    else:
+        print("Unable to fetch live stream details!")
+        sys.exit(1)
+
+
+def print_live_stream_details(encoding_id, live_stream_details):
+    rtmp_service="enc-{}-bitcodin-rtmp".format(encoding_id)
+    node_port_patch_command="kubectl patch service {} -p '{{ \"spec\": {{ \"type\": \"NodePort\" }} }}'".format(rtmp_service)
+    query_node_port="kubectl get service {} -o json | jq '.spec.ports[] | select(.name==\"rtmp-ingest-point\").nodePort'".format(rtmp_service)
+    node_port_patch_custom_id="kubectl patch service {} -p '{{ \"spec\": {{ \"type\": \"NodePort\" \"nodePort\": \"30001\" }} }}'".format(rtmp_service)
+    load_balancer="kubectl patch service {} -p '{{ \"spec\": {{ \"type\": \"LoadBalancer\" }} }}'".format(rtmp_service)
+    load_balancer_ip_and_port="kubectl get service {} -o json | jq -r '.status.loadBalancer.ingress[] | .ip'".format(rtmp_service)
+
+    print('\n-----------------------------------------------------')
+    print('Live Stream set up successfully: \n')
+    print('Encoding ID ... {}'.format(encoding_id))
+    print('Encoder IP .... {}'.format(live_stream_details.encoderIp))
+    print('Stream Key .... {}'.format(live_stream_details.streamKey))
+    print('')
+    print('Stream URL: ... {}'.format('rtmp://<public-ip>:<port>/live'.format(live_stream_details.encoderIp)))
+    print('-----------------------------------------------------')
+    print('You can now configure the RTMP ingest point for access outside of your infrastructure.')
+    print('Configure node port access ............................ {}'.format(node_port_patch_command))
+    print('Configure node port access (custom port) .............. {}'.format(node_port_patch_custom_id))
+    print('Find the node port .................................... {}'.format(query_node_port))
+    print('Configure public load balancer ........................ {}'.format(load_balancer))
+    print('Find out the ip of the load balancer (use port 1935)... {}'.format(load_balancer_ip_and_port))
+    print('\n')
+
+
+if __name__ == '__main__':
+    main()
