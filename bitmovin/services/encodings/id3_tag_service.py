a60b33f54673cba19f74fa031d02043d687716d1
dominic-miglar <dominic.miglar@netunix.at>
2017-05-31 11:46:58 +0200

    added id3 services

---
+++ bitmovin/services/encodings/id3_tag_service.py
@@ -0,0 +1,5 @@
+
+class ID3TagService:
+    def __init__(self, http_client):
+        super().__init__()
+        self.http_client = http_client
--- bitmovin/services/encodings/progressive_ts_muxing_service.py
+++ bitmovin/services/encodings/progressive_ts_muxing_service.py
@@ -1,8 +1,10 @@
 from bitmovin.resources.models import ProgressiveTSMuxing as ProgressiveTSMuxingResource
 from .generic_muxing_service import GenericMuxingService
+from .progressive_ts_id3_service import ProgressiveTSID3Service
 
 
 class ProgressiveTSMuxing(GenericMuxingService):
 
     def __init__(self, http_client):
         super().__init__(http_client=http_client, type_url='progressive-ts', resource_class=ProgressiveTSMuxingResource)
+        self.ID3Tags = ProgressiveTSID3Service(http_client=http_client)
