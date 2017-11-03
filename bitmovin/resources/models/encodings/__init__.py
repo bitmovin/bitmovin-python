def51db483179b6fe68afefa037d74faf83c6b42
dmoser <david.moser@bitmovin.com>
2017-11-03 10:05:41 +0100

    Reverted __init__.py in models.encoding

--- bitmovin/resources/models/encodings/__init__.py
+++ bitmovin/resources/models/encodings/__init__.py
@@ -1,19 +1,18 @@
+from .muxings import Muxing, FMP4Muxing, MP4Muxing, TSMuxing, WebMMuxing, ProgressiveTSMuxing, MuxingStream
+from .drms import DRM, DRMStatus, AESDRM, ClearKeyDRM, FairPlayDRM, MarlinDRM, PlayReadyDRM, PrimeTimeDRM, WidevineDRM, CENCDRM, CENCPlayReadyEntry, CENCWidevineEntry
 from .acl_entry import ACLEntry
-from .drms import DRM, DRMStatus, AESDRM, ClearKeyDRM, FairPlayDRM, MarlinDRM, PlayReadyDRM, PrimeTimeDRM, WidevineDRM, \
-    CENCDRM, CENCPlayReadyEntry, CENCWidevineEntry
 from .encoding import Encoding
-from .encoding_live_details import EncodingLiveDetails
 from .encoding_output import EncodingOutput
 from .encoding_status import EncodingStatus
-from .id3 import ID3Tag, RawID3Tag, FrameIdID3Tag, PlainTextID3Tag
-from .ignored_by import IgnoredBy
-from .ignored_by_type import IgnoredByType
-from .keyframe import Keyframe
-from .live import LiveHlsManifest, LiveDashManifest, LiveStreamConfiguration
-from .muxings import Muxing, FMP4Muxing, MP4Muxing, TSMuxing, WebMMuxing, ProgressiveTSMuxing, MuxingStream
-from .sprite import Sprite
-from .start import StartEncodingRequest, StartEncodingTrimming
 from .stream import Stream
-from .stream_filter import StreamFilter
 from .stream_input import StreamInput
+from .encoding_live_details import EncodingLiveDetails
+from .live import LiveHlsManifest, LiveDashManifest, LiveStreamConfiguration
 from .thumbnail import Thumbnail
+from .sprite import Sprite
+from .stream_filter import StreamFilter
+from .keyframe import Keyframe
+from .id3 import ID3Tag, RawID3Tag, FrameIdID3Tag, PlainTextID3Tag
+from .ignored_by_type import IgnoredByType
+from .ignored_by import IgnoredBy
+from .start import StartEncodingRequest, StartEncodingTrimming
