87e917e5bb76bef4719e30a8897e12e9487641d5
dmoser <david.moser@bitmovin.com>
2018-03-21 18:28:40 +0100

    Added text_filter to __init__.py Added text_filter_service

--- bitmovin/resources/models/filters/__init__.py
+++ bitmovin/resources/models/filters/__init__.py
@@ -7,3 +7,4 @@ from .audio_mix_channel import AudioMixChannel
 from .audio_mix_filter import AudioMixFilter
 from .deinterlace_filter import DeinterlaceFilter
 from .denoise_hqdn3d_filter import DenoiseHqdn3dFilter
+from .text_filter import TextFilter
