98d1ecb7460f6925522ed518ec9185ac0c8681d5
dmoser <david.moser@bitmovin.com>
2018-04-10 12:55:53 +0200

    Removed default value

--- bitmovin/resources/enums/input_color_range.py
+++ bitmovin/resources/enums/input_color_range.py
@@ -5,7 +5,3 @@ class InputColorRange(enum.Enum):
     UNSPECIFIED = 'UNSPECIFIED'
     MPEG = 'MPEG'
     JPEG = 'JPEG'
-
-    @staticmethod
-    def default():
-        return InputColorRange.UNSPECIFIED
