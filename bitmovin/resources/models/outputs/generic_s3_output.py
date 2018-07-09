f423e8108cfd6454a8bde35a8f314b698ad30f5c
Matt Lee <matt.lee@bitmovin.com>
2018-07-09 14:33:56 +0800

    Added the Ability to Set S3SignatureVersion and SSL on Generic S3 Resources

--- bitmovin/resources/models/outputs/generic_s3_output.py
+++ bitmovin/resources/models/outputs/generic_s3_output.py
@@ -1,4 +1,5 @@
 from bitmovin.utils import Serializable
+from bitmovin.resources.enums import S3SignatureVersion
 from . import AbstractOutput
 
 
@@ -7,6 +8,9 @@ class GenericS3Output(AbstractOutput, Serializable):
     def __init__(self, access_key, secret_key, bucket_name, host, port=None, signature_version=None, ssl=None, id_=None, custom_data=None,
                  name=None, description=None):
         super().__init__(id_=id_, custom_data=custom_data, name=name, description=description)
+        
+        self._signatureVersion = None
+        
         self.accessKey = access_key
         self.secretKey = secret_key
         self.bucketName = bucket_name
@@ -14,6 +18,22 @@ class GenericS3Output(AbstractOutput, Serializable):
         self.port = port
         self.signatureVersion = signature_version
         self.ssl = ssl
+
+    @property
+    def signatureVersion(self):
+        return self._signatureVersion
+
+    @signatureVersion.setter
+    def signatureVersion(self, new_sigver):
+        if new_sigver is None:
+            return
+        if isinstance(new_sigver, str):
+            self._signatureVersion = new_sigver
+        elif isinstance(new_sigver, S3SignatureVersion):
+            self._signatureVersion = new_sigver.value
+        else:
+            raise InvalidTypeError(
+                'Invalid type {} for signatureVersion: must be either str or S3SignatureVersion!'.format(type(new_signatureVersion)))
 
     @classmethod
     def parse_from_json_object(cls, json_object):
@@ -34,4 +54,5 @@ class GenericS3Output(AbstractOutput, Serializable):
 
     def serialize(self):
         serialized = super().serialize()
+        serialized['signatureVersion'] = self.signatureVersion
         return serialized
