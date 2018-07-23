8156e7b729bc3909ced908d2f71a24139f909429
jonathanperry <jonathan.perry@bitmovin.com>
2018-07-23 14:53:22 -0700

    Add TimeCode Support

--- bitmovin/resources/models/encodings/muxings/mp4_muxing.py
+++ bitmovin/resources/models/encodings/muxings/mp4_muxing.py
@@ -4,11 +4,12 @@ from .muxing import Muxing
 class MP4Muxing(Muxing):
 
     def __init__(self, streams, filename=None, outputs=None, id_=None, custom_data=None, name=None, description=None,
-                 ignored_by=None, fragment_duration=None):
+                 ignored_by=None, fragment_duration=None, time_code=None):
         super().__init__(id_=id_, custom_data=custom_data, streams=streams, outputs=outputs,
                          name=name, description=description, ignored_by=ignored_by)
         self.filename = filename
         self.fragmentDuration = fragment_duration
+        self.timeCode = time_code
 
     @classmethod
     def parse_from_json_object(cls, json_object):
@@ -24,8 +25,9 @@ class MP4Muxing(Muxing):
 
         filename = json_object['filename']
         fragment_duration = json_object.get('fragmentDuration')
+        time_code = json_object.get('timeCode')
 
         mp4_muxing = MP4Muxing(streams=streams, filename=filename, outputs=outputs, id_=id_, custom_data=custom_data,
                                name=name, description=description, ignored_by=ignored_by,
-                               fragment_duration=fragment_duration)
+                               fragment_duration=fragment_duration, time_code=time_code)
         return mp4_muxing
---
+++ bitmovin/resources/models/encodings/muxings/time_code.py
@@ -0,0 +1,4 @@
+class TimeCode:
+
+    def __init__(self, time_code_start):
+        self.timeCodeStart = time_code_start
