1f85cdae7510d2c67b18df079c5440ffc25918ed
Dominic Miglar <dominic.miglar@netunix.at>
2018-08-18 21:29:42 +0200

    added encoding_mode to encoding start request

--- bitmovin/resources/enums/__init__.py
+++ bitmovin/resources/enums/__init__.py
@@ -48,3 +48,4 @@ from .h264_interlace_mode import H264InterlaceMode
 from .s3_sig_version import S3SignatureVersion
 from .watermark_unit import WatermarkUnit
 from .he_aac_signaling import HeAacSignaling
+from .encoding_mode import EncodingMode
--- bitmovin/resources/models/encodings/start/start_encoding_request.py
+++ bitmovin/resources/models/encodings/start/start_encoding_request.py
@@ -1,4 +1,5 @@
 from bitmovin.errors import InvalidTypeError
+from bitmovin.resources.enums import EncodingMode
 from bitmovin.utils import Serializable
 
 from .start_encoding_trimming import StartEncodingTrimming
@@ -6,12 +7,14 @@ from .scheduling import Scheduling
 
 
 class StartEncodingRequest(Serializable):
-    def __init__(self, trimming=None, scheduling=None):
+    def __init__(self, trimming=None, scheduling=None, encoding_mode=None):
         super().__init__()
+        self._encoding_mode = None
         self._trimming = None
         self._scheduling = None
         self.trimming = trimming
         self.scheduling = scheduling
+        self.encodingMode = encoding_mode
 
     @property
     def trimming(self):
@@ -48,8 +51,26 @@ class StartEncodingRequest(Serializable):
 
         self._scheduling = new_scheduling
 
+    @property
+    def encodingMode(self):
+        return self._encoding_mode
+
+    @encodingMode.setter
+    def encodingMode(self, new_encoding_mode):
+        if new_encoding_mode is None:
+            self._encoding_mode = None
+            return
+        if isinstance(new_encoding_mode, str):
+            self._encoding_mode = new_encoding_mode
+        elif isinstance(new_encoding_mode, EncodingMode):
+            self._encoding_mode = new_encoding_mode.value
+        else:
+            raise InvalidTypeError(
+                'Invalid type {} for encodingMode: must be either str or EncodingMode!'.format(type(new_encoding_mode)))
+
     def serialize(self):
         serialized = super().serialize()
         serialized['trimming'] = self.trimming
         serialized['scheduling'] = self.scheduling
+        serialized['encodingMode'] = self.encodingMode
         return serialized
