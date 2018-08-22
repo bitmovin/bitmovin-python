eb7a6d4036737e8ef8f969a9ba1cc239fa8054b4
Dominic Miglar <dominic.miglar@netunix.at>
2018-08-22 10:25:51 +0200

    added tweaks test to encoding_start_tests

--- tests/bitmovin/services/encodings/encoding_start_tests.py
+++ tests/bitmovin/services/encodings/encoding_start_tests.py
@@ -1,6 +1,6 @@
 import unittest
 import json
-from bitmovin import Bitmovin, StartEncodingRequest, Scheduling
+from bitmovin import Bitmovin, StartEncodingRequest, Scheduling, Tweaks, AudioVideoSyncMode
 from tests.bitmovin import BitmovinTestCase
 from bitmovin.utils import BitmovinJSONEncoder
 
@@ -34,6 +34,16 @@ class EncodingStartTests(BitmovinTestCase):
         start_encoding_request = StartEncodingRequest(scheduling=scheduling)
         start_encoding_request_serialized = json.dumps(obj=start_encoding_request, cls=BitmovinJSONEncoder)
         self.assertEqual(first=start_encoding_request_serialized, second='{"scheduling": {"prewarmedInstancePoolIds": ["4a67260e-2fd3-4e9e-9829-651280ea8f06", "4b67260e-2fd3-4e9e-9829-651280ea8f07"]}}')
+
+    def test_encoding_start_request_serialization_with_tweaks(self):
+        tweaks = Tweaks(audio_video_sync_mode=AudioVideoSyncMode.RESYNC_AT_START)
+
+        tweaks_serialized = json.dumps(obj=tweaks, cls=BitmovinJSONEncoder)
+        self.assertEqual(first=tweaks_serialized, second='{"audioVideoSyncMode": "RESYNC_AT_START"}')
+
+        start_encoding_request = StartEncodingRequest(tweaks=tweaks)
+        start_encoding_request_serialized = json.dumps(obj=start_encoding_request, cls=BitmovinJSONEncoder)
+        self.assertEqual(first=start_encoding_request_serialized, second='{"tweaks": {"audioVideoSyncMode": "RESYNC_AT_START"}}')
 
 
 if __name__ == '__main__':
