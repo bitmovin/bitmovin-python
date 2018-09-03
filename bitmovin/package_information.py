62945bbbacc8bc6cc94aa134d10b46bfb7bf9cbb
Dominic Miglar <dominic.miglar@netunix.at>
2018-09-03 18:06:40 +0200

    lvl up: v1.33.0

--- bitmovin/package_information.py
+++ bitmovin/package_information.py
@@ -1,2 +1,2 @@
 NAME = 'bitmovin-python'
-VERSION = '1.33.0.dev0'
+VERSION = '1.33.0'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from setuptools import setup, find_packages
 
 __name__ = 'bitmovin-python'
-__version__ = '1.33.0.dev0'
+__version__ = '1.33.0'
 __description__ = 'Python wrapper for the bitmovin API'
 __author__ = 'Dominic Miglar <dominic.miglar@bitmovin.com>'
 __email__ = 'dominic.miglar@bitmovin.com'
