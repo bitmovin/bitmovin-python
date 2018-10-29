4ea73d4ea904e97f5390a499d1ea27b260134df5
Dominic Miglar <dominic.miglar@netunix.at>
2018-10-29 13:39:25 +0100

    fixed custom_xml_element

--- bitmovin/resources/models/manifests/dash/custom_xml_element.py
+++ bitmovin/resources/models/manifests/dash/custom_xml_element.py
@@ -1,13 +1,18 @@
-from bitmovin.utils import Serializable
+from bitmovin.resources.models import AbstractModel
 
 
-class CustomXMLElement(Serializable):
+class CustomXMLElement(AbstractModel):
 
-    def __init__(self, data):
-        super().__init__()
+    def __init__(self, data, id_=None, custom_data=None):
+        super().__init__(id_=id_, custom_data=custom_data)
         self.data = data
 
     @classmethod
     def parse_from_json_object(cls, json_object):
+        id_ = json_object.get('id')
+        custom_data = json_object.get('customData')
         data = json_object.get('data')
-        return CustomXMLElement(data=data)
+
+        custom_xml_element = CustomXMLElement(id_=id_, custom_data=custom_data, data=data)
+
+        return custom_xml_element
