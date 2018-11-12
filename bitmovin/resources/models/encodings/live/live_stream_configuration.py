4be16c69c0db11fc44afbe9290951ec647289c46
Dominic Miglar <dominic.miglar@netunix.at>
2018-11-12 14:49:15 +0100

    cosmetics

--- bitmovin/resources/models/encodings/live/live_stream_configuration.py
+++ bitmovin/resources/models/encodings/live/live_stream_configuration.py
@@ -1,15 +1,17 @@
 from typing import List
-
 from bitmovin.utils import Serializable
+from bitmovin.resources.enums import LiveEncodingMode
+from bitmovin.errors import InvalidTypeError
 from .live_dash_manifest import LiveDashManifest
 from .live_hls_manifest import LiveHlsManifest
-from bitmovin.resources.enums import LiveEncodingMode
+
 
 class LiveStreamConfiguration(Serializable):
-    def __init__(self, stream_key: str,
+    def __init__(self,
+                 stream_key: str,
                  live_dash_manifests: List[LiveDashManifest] = None,
                  live_hls_manifests: List[LiveHlsManifest] = None,
-                 live_encoding_mode = None):
+                 live_encoding_mode=None):
         super().__init__()
         self.streamKey = stream_key
         self.dashManifests = live_dash_manifests
@@ -25,14 +27,14 @@ class LiveStreamConfiguration(Serializable):
     def liveEncodingMode(self, new_encoding_mode):
         if new_encoding_mode is None:
             self._live_encoding_mode = None
-            return
-        if isinstance(new_encoding_mode, str):
+        elif isinstance(new_encoding_mode, str):
             self._live_encoding_mode = new_encoding_mode
         elif isinstance(new_encoding_mode, LiveEncodingMode):
             self._live_encoding_mode = new_encoding_mode.value
         else:
             raise InvalidTypeError(
-                'Invalid type {} for liveEncodingMode: must be either str or LiveEncodingMode!'.format(type(new_encoding_mode)))
+                'Invalid type {} for liveEncodingMode: must be either str or LiveEncodingMode!'.format(
+                    type(new_encoding_mode)))
 
     def serialize(self):
         serialized = super().serialize()
