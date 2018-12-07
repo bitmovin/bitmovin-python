ffc733919def57423c01c14c7acf1c4863d09462
wabiloo <fabre.lambeau@gmail.com>
2018-12-07 18:53:06 +0000

    Adding signature version param for S3 Outputs

--- bitmovin/resources/models/outputs/s3_output.py
+++ bitmovin/resources/models/outputs/s3_output.py
@@ -1,5 +1,5 @@
 from bitmovin.errors import InvalidTypeError
-from bitmovin.resources.enums import AWSCloudRegion
+from bitmovin.resources.enums import AWSCloudRegion, S3SignatureVersion
 from bitmovin.utils import Serializable
 from . import AbstractOutput
 
@@ -7,7 +7,7 @@ from . import AbstractOutput
 class S3Output(AbstractOutput, Serializable):
 
     def __init__(self, access_key, secret_key, bucket_name, cloud_region=None, id_=None, custom_data=None,
-                 name=None, description=None, md5_meta_tag=None):
+                 name=None, description=None, md5_meta_tag=None, signature_version=None):
         super().__init__(id_=id_, custom_data=custom_data, name=name, description=description)
         self.accessKey = access_key
         self.secretKey = secret_key
@@ -15,6 +15,8 @@ class S3Output(AbstractOutput, Serializable):
         self._cloudRegion = None
         self.cloudRegion = cloud_region
         self.md5MetaTag = md5_meta_tag
+        self._signatureVersion = None
+        self.signatureVersion = signature_version
 
     @property
     def cloudRegion(self):
@@ -32,6 +34,24 @@ class S3Output(AbstractOutput, Serializable):
             raise InvalidTypeError(
                 'Invalid type {} for cloudRegion: must be either str or AWSCloudRegion!'.format(type(new_region)))
 
+    @property
+    def signatureVersion(self):
+        return self._signatureVersion
+
+    @signatureVersion.setter
+    def signatureVersion(self, new_signature_version):
+        if new_signature_version is None:
+            self._signatureVersion = None
+        elif isinstance(new_signature_version, str):
+            self._signatureVersion = new_signature_version
+        elif isinstance(new_signature_version, S3SignatureVersion):
+            self._signatureVersion = new_signature_version.value
+        else:
+            raise InvalidTypeError(
+                'Invalid type {} for signatureVersion: must be either str of S3SignatureVersion!'.
+                    format(type(new_signature_version))
+            )
+
     @classmethod
     def parse_from_json_object(cls, json_object):
         id_ = json_object['id']
@@ -42,13 +62,15 @@ class S3Output(AbstractOutput, Serializable):
         name = json_object.get('name')
         description = json_object.get('description')
         md5_meta_tag = json_object.get('md5MetaTag')
+        signature_version = json_object.get('signatureVersion')
 
         s3_output = S3Output(
             access_key=access_key, secret_key=secret_key, bucket_name=bucket_name, cloud_region=cloud_region, id_=id_,
-            name=name, description=description, md5_meta_tag=md5_meta_tag)
+            name=name, description=description, md5_meta_tag=md5_meta_tag, signature_version=signature_version)
         return s3_output
 
     def serialize(self):
         serialized = super().serialize()
         serialized['cloudRegion'] = self.cloudRegion
+        serialized['signatureVersion'] = self.signatureVersion
         return serialized
