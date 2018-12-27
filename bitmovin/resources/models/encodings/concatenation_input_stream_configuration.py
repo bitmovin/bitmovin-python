bfa2634fccf5ab1cae77f68e2fa4cd545fb39955
Dominic Miglar <dominic.miglar@netunix.at>
2018-12-27 13:58:48 +0100

    cosmetics

--- bitmovin/resources/models/encodings/concatenation_input_stream_configuration.py
+++ bitmovin/resources/models/encodings/concatenation_input_stream_configuration.py
@@ -1,32 +1,29 @@
 from bitmovin.utils import Serializable
-from bitmovin.resources import AbstractIdResource
+from bitmovin.resources import Resource
 
 
-class ConcatenationInputStreamConfiguration(AbstractIdResource, Serializable):
+class ConcatenationInputStreamConfiguration(Resource, Serializable):
 
-    def __init__(self, input_stream_id, is_main, id_=None, position=None):
-        super().__init__(id_=id_)
-
-        self._position = None
+    def __init__(self, input_stream_id, is_main, position=None):
+        super().__init__()
         self.inputStreamId = input_stream_id
         self.isMain = is_main
         self.position = position
 
     @classmethod
     def parse_from_json_object(cls, json_object):
-        id_ = json_object.get('id')
-        input_stream_id = json_object['inputStreamId']
-        is_main = json_object['isMain']
+        input_stream_id = json_object.get('inputStreamId')
+        is_main = json_object.get('isMain')
         position = json_object.get('position')
 
-        concatentation_input_stream_configuration = ConcatenationInputStreamConfiguration(input_stream_id=input_stream_id, is_main=is_main, id_=id_,
-                                   position=position)
+        concatentation_input_stream_configuration = ConcatenationInputStreamConfiguration(
+            input_stream_id=input_stream_id,
+            is_main=is_main,
+            position=position
+        )
 
         return concatentation_input_stream_configuration
 
     def serialize(self):
         serialized = super().serialize()
-        serialized['inputStreamId'] = self.inputStreamId
-        serialized['isMain'] = self.isMain
-        serialized['position'] = self.position
         return serialized
