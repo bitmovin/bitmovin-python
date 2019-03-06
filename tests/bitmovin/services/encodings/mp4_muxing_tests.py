fd194981a1a544333bf6818a2babd465a0b19524
Dominic Miglar <dominic.miglar@netunix.at>
2019-03-06 11:21:54 +0100

    fixed tests

--- tests/bitmovin/services/encodings/mp4_muxing_tests.py
+++ tests/bitmovin/services/encodings/mp4_muxing_tests.py
@@ -179,7 +179,7 @@ class EncodingMP4MuxingTests(BitmovinTestCase):
     def test_create_internal_chunk_length_quality_optimized(self):
         sample_muxing = self._get_sample_muxing()
 
-        sample_muxing.internal_chunk_length = InternalChunkLength(mode=InternalChunkLengthMode.QUALITY_OPTIMIZED,
+        sample_muxing.internal_chunk_length = InternalChunkLength(mode=InternalChunkLengthMode.CUSTOM,
                                                                   custom_chunk_length=None)
 
         muxing_resource_response = self.bitmovin.encodings.Muxing.MP4.create(object_=sample_muxing,
@@ -189,13 +189,13 @@ class EncodingMP4MuxingTests(BitmovinTestCase):
         self.assertIsNotNone(muxing_resource_response.resource.id)
         self._compare_muxings(sample_muxing, muxing_resource_response.resource)
 
-        self.assertEqual(InternalChunkLengthMode.QUALITY_OPTIMIZED.value,
+        self.assertEqual(InternalChunkLengthMode.CUSTOM.value,
                          muxing_resource_response.resource.internal_chunk_length.mode)
 
     def test_create_muxing_with_internal_chunk_length(self):
         sample_muxing = self._get_sample_muxing()
 
-        internal_chunk_length = InternalChunkLength(mode=InternalChunkLengthMode.QUALITY_OPTIMIZED,
+        internal_chunk_length = InternalChunkLength(mode=InternalChunkLengthMode.CUSTOM,
                                                     custom_chunk_length=12.345)
 
         sample_muxing.internal_chunk_length = internal_chunk_length
