8e423ba5548b5b614f0d6195ab162a6ca840dd00
Dominic Miglar <dominic.miglar@netunix.at>
2019-03-29 13:56:30 +0100

    added caption_character_encoding

--- bitmovin/resources/enums/__init__.py
+++ bitmovin/resources/enums/__init__.py
@@ -74,3 +74,4 @@ from .h265_adaptive_quantization_mode import H265AdaptiveQuantizationMode
 from .hls_version import HlsVersion
 from .stream_conditions_mode import StreamConditionsMode
 from .internal_chunk_length_mode import InternalChunkLengthMode
+from .caption_character_encoding import CaptionCharacterEncoding
